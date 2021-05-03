55ef6a0dc7d8ce1ea9be826cccec137f1a6d5461
John Edrick <edricknubla25@gmail.com>
2021-05-03 21:31:01 +0800

    update3

--- src/components/login.js
+++ src/components/login.js
@@ -2,10 +2,18 @@ function Login() {
   return (
     <div className="login-page">
       <div className="login-container">
-        <h3>Login</h3>
+        <h3>Sign in</h3>
         <form>
-          <input type="text"></input>
-          <input type="text"></input>
+          <input type="text" placeholder="Username/Email"></input>
+          <input type="text" placeholder="Password"></input>
+          <a href="#e">
+            <u>Forgot Password?</u>
+          </a>
+          <button>Submit</button>
+          <hr />
+          <a href="#e" className="create-account">
+            Create an account
+          </a>
         </form>
       </div>
     </div>
